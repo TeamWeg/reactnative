98d4ce930a51c02c6a82ff444cd3d1952379a3c5
Will Anderson <wllmscttandrsn@gmail.com>
2018-10-15 01:05:30 -0400

    yeet

--- reactproject/App.js
+++ reactproject/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Yeet name="Will"/>
+      </View>
+    );
+  }
+}
+
+class Yeet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowing: true,
+    };
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowing: !previousState.isShowing };
+      });
+    }, 100)
+  }
+  render() {
+    let pic = {
+      'uri': 'https://upload.wikimedia.org/wikipedia/commons/2/22/Joseph_Campbell_Company_%283093577454%29.jpg'
+    };
+    let display = this.state.isShowing ? <Text>Yeet, {this.props.name}!</Text> : '';
+    return (
+      <View style={styles.container}>
+        <Text>{display}</Text>
+        <View>
+          <Image source={pic} style={{height:200, width:150}}/>
+        </View>
       </View>
     );
   }
